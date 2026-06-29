d33fc4ae588e4a59449733ffe4bed1f94d1c72a6
garvmar <grv.m027@gmail.com>
2026-06-29 14:27:58 +0300

    data auto

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 import cv2
 from ultralytics import YOLO
 
-model = YOLO('best.pt').to('cpu')
+model = YOLO('best1.pt').to('cpu')
 
-image_path = "test_image2.jpg"  
+image_path = "test_image3.jpg"  
 
 # Распознать объекты
 results = model(image_path)
